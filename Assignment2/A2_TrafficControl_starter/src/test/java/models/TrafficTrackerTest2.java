31dec3be39b6377955a0bf71d9141e0a58892c3e
Romello <romello.ten.broeke@hva.nl>
2023-10-15 19:47:38 +0200

    Additional tests

--- Assignment2/A2_TrafficControl_starter/src/test/java/models/TrafficTrackerTest2.java
+++ Assignment2/A2_TrafficControl_starter/src/test/java/models/TrafficTrackerTest2.java
@@ -17,8 +17,18 @@ public class TrafficTrackerTest2 extends TrafficTrackerTest{
     }
 
     @Test
-    public void assertOutofBounds() {
+    public void assertOutOfBounds() {
         Assertions.assertThrows(IndexOutOfBoundsException.class, () -> trafficTracker.topViolationsByCar(3));
+    }
+
+    @Test
+    public void assertNumberPlate() {
+        Assertions.assertTrue(trafficTracker.topViolationsByCar(1).toString().contains("227-HX-3"));
+    }
+
+    @Test
+    public void assertOneCarViolations() {
+        Assertions.assertTrue(trafficTracker.topViolationsByCar(1).toString().contains("7"));
     }
 
     @Test
